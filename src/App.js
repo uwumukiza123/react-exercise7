381428b3669bbb4c3fb538089ded529df5bed9ea
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-01 19:55:36 +0200

    add react exercise7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
+  const [addInput, setInput] = React.useState("");
+  const [addInput1, setInput1] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello ${addInput} ${addInput1}!`);
+  };
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  function handleChange1(e) {
+    setInput1(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          value={addInput}
+          type="text"
+          name=""
+          id=""
+          placeholder="first name"
+          onChange={handleChange}
+        />
+        <input
+          className="input"
+          value={addInput1}
+          type="text"
+          name=""
+          id=""
+          placeholder="last name"
+          onChange={handleChange1}
+        />
+        <button>Click Me</button>
+      </form>
     </div>
   );
 }
